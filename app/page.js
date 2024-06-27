1015ef108c8a35309b187a0c7ad043eefd0818f0
Akash Singh <88923478+AkashSingh2002@users.noreply.github.com>
2024-06-27 06:33:37 +0000

    folder structuring

--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
 // pages/index.js
 import React from 'react';
 import Head from 'next/head';
-import Home from './Home';
+import Home from './pages/Home';
+import LoginPage from './pages/Login';
+import RegisterPage from './pages/Register';
+import NoirRewards from './pages/Noir_coin';
+import AboutUs from './pages/about';
 
 const HomePage = () => {
   return (
     <>
-      <Home />
+      <AboutUs />
     </>
   );
 }
