eb22e689f7d335fa318a2079b16405da28a5706d
Akash Singh <88923478+AkashSingh2002@users.noreply.github.com>
2024-07-05 13:06:52 +0000

    theme song

--- app/components/Header.js
+++ app/components/Header.js
@@ -86,7 +86,7 @@ const Header = () => {
               </li>
             )}
             <li className="relative group z-10">
-              <Link href="#" className="text-white hover:text-blue-500 transition">Shop</Link>
+              <Link href="/pages/shop" className="text-white hover:text-blue-500 transition">Shop</Link>
               <ul className="absolute left-0 mt-1 bg-gray-800 hidden group-hover:block z-10">
                 <li><Link href="#" className="block px-4 py-2 text-white hover:bg-gray-700">Merchandise</Link></li>
                 <li><Link href="#" className="block px-4 py-2 text-white hover:bg-gray-700">In-Game Items</Link></li>
