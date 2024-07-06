f4d323ee323907aabedd7ffec34bf394e2000a64
Akash Singh <88923478+AkashSingh2002@users.noreply.github.com>
2024-07-06 07:19:30 +0000

    implemented the music player

--- app/pages/captain-roger-game/page.js
+++ app/pages/captain-roger-game/page.js
@@ -2,9 +2,18 @@
 "use client"
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
+import MusicPlayer from '@/app/components/MusicPlayer';
 
 const CTD = () => {
   const [score, setScore] = useState(0);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const tracks = [
+    { src: '/music/2_Ciphers.mp3', title: '2_Ciphers' },
+    { src: '/music/Decentralization.mp3', title: 'Decentralization' },
+    { src: '/music/Digital_Revolution.mp3', title: 'Digital_Revolution' },
+    { src: '/music/Game_On.mp3', title: 'Game_on' }
+  ];
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -17,24 +26,37 @@ const CTD = () => {
     return () => clearInterval(interval);
   }, []);
 
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
-    <div className="container" style={{ width: '100%', height: '100vh', backgroundColor: 'lightpink', display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden' }}>
+    <div className="flex w-full h-screen bg-purple-700 overflow-hidden">
       <Head>
         <title>Connect The Dots Game</title>
         <meta name="description" content="Play Connect The Dots Game" />
       </Head>
-      <main style={{ border: '5px solid #000', borderRadius: '10px', overflow: 'hidden', width: '95%', height: '95%', boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)', position: 'relative' }}>
+      <main className={`flex-grow relative border-5 border-black rounded-10 overflow-hidden shadow-lg ${isCollapsed ? 'w-full md:w-3/4' : 'w-full md:w-9/12'}`}>
         <iframe
           id="gameFrame"
           src="/Captain-Rogers-master/index.html" // Adjust the path to your HTML file in the public folder
-          width="100%"
-          height="100%"
+          className="w-full h-full"
           style={{ border: 'none', backgroundColor: 'black' }} // Set background color to black
         ></iframe>
-        <div style={{ position: 'absolute', top: '10px', right: '10px', color: 'white', backgroundColor: 'rgba(0, 0, 0, 0.5)', padding: '5px 10px', borderRadius: '5px' }}>
+        <div className="absolute top-2 right-2 text-white bg-black bg-opacity-50 p-2 rounded">
           Score: {score}
         </div>
       </main>
+      <div className={`flex-shrink-0 h-full flex flex-col items-center justify-center ${isCollapsed ? 'hidden md:flex' : 'flex'}`}
+  style={{
+    backgroundImage: `url('/game.jpg')`,  // Replace with your background image URL
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  }}
+>
+  <MusicPlayer tracks={tracks} isCollapsed={isCollapsed} toggleCollapse={toggleCollapse} />
+</div>
+
     </div>
   );
 };
