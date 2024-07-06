2287a389e092dfc008549ba624f2a8261da77fd9
pranshum21 <119186276+pranshum21@users.noreply.github.com>
2024-07-06 18:06:52 +0530

    Delete backend directory

--- backend/api/routes/gameRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const gameController = require('../controllers/gameController');
-const authenticateToken = require('../../config/authMiddleware');
-
-router.post('/', authenticateToken, gameController.createGame);
-router.get('/', gameController.getGames);
-
-module.exports = router;
